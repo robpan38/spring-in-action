10515c6ddf04146a842077eb4e6806ba19bfb0bb
Robert Pandele <r.pandele38@gmail.com>
2023-04-11 19:07:54 +0300

    feat: add get request handling + view for /request

---
+++ src/main/java/sia/tacocloud/controllers/DesignTacoController.java
@@ -0,0 +1,77 @@
+package sia.tacocloud.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import sia.tacocloud.data_model.Ingredient;
+import sia.tacocloud.data_model.Taco;
+import sia.tacocloud.data_model.TacoOrder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// automatically create a static logger instance in the class
+@Slf4j
+// create a DesignTacoController bean in the Spring Context
+@Controller
+// handle requests whose path begins with /design
+@RequestMapping("/design")
+// make sure this attribute is maintained in the session
+@SessionAttributes("tacoOrder")
+public class DesignTacoController {
+    // the model carries data between the controller and the view
+    // model's attributes will end up in the attributes of the request
+    // allowing the view to display data
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
+        );
+
+        Ingredient.Type[] types = Ingredient.Type.values();
+        for (Ingredient.Type type : types) {
+            model.addAttribute(
+                    type.toString().toLowerCase(),
+                    filterByType(ingredients, type)
+            );
+        }
+    }
+
+    @ModelAttribute(name = "tacoOrder")
+    public TacoOrder order() {
+        return new TacoOrder();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
+    }
+
+    // handle get request at /design
+    @GetMapping
+    public String showDesignForm() {
+        // return the logical name of the view
+        return "design";
+    }
+
+    private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Ingredient.Type type) {
+        return ingredients
+                .stream()
+                .filter(ingredient -> ingredient.getType() == type)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/sia/tacocloud/data_model/Taco.java
@@ -0,0 +1,10 @@
+package sia.tacocloud.data_model;
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<Ingredient> ingredients;
+}
